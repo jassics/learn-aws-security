a3f2951623365df61423c7a86c772dab944e0235
Sanjeev Jaiswal <jassics@gmail.com>
2018-08-28 18:51:56 +0530

    2 more scripts added

---
+++ aws-sgname-publicip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import boto3
+import json
+
+all_ec2 = {}
+filters = [{'Name':'instance.group-name', 'Values':['*preprod*']}]
+
+# To list all the available regions in AWS
+region_names = [x['RegionName'] for x in boto3.client('ec2').describe_regions()['Regions']]
+
+# print security group which contains preprod keyword and associated public IP
+# address, if exists
+for region_name in region_names:
+    print("Looking for instances with security group having preprod keyword in region: " + region_name + " .")
+    for reservation in boto3.client('ec2', region_name=region_name).describe_instances(Filters=filters)['Reservations']:
+        for instance in reservation['Instances']:
+            for sg in instance.get('SecurityGroups', []):
+                all_ec2[instance['InstanceId']] = instance
+                print(sg)
+                print(instance['PrivateIpAddress'])
+
---
+++ ec2_info.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+import boto3
+
+
+# Instance count client interface
+ec2 = boto3.client('ec2', region_name='us-east-1')
+idesc = ec2.describe_instances()
+total = len(idesc['Reservations'])
+print ("Total: ", total)
+
+# Resource Interface 
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+for instance in ec2.instances.all():
+    print (instance, instance.tags)
+
+
+"""
+A tool for retrieving basic information from the running EC2 instances.
+"""
+
+# Connect to EC2
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+# Get information for all running instances
+running_instances = ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['running']}])
+
+ec2info = defaultdict()
+for instance in running_instances:
+    for tag in instance.tags:
+        if 'Name'in tag['Key']:
+            name = tag['Value']
+    # Add instance info to a dictionary         
+    ec2info[instance.id] = {
+        'Name': name,
+        'Type': instance.instance_type,
+        'State': instance.state['Name'],
+        'Private IP': instance.private_ip_address,
+        'Public IP': instance.public_ip_address,
+        'Launch Time': instance.launch_time
+        }
+
+attributes = ['Name', 'Type', 'State', 'Private IP', 'Public IP', 'Launch Time']
+for instance_id, instance in ec2info.items():
+    for key in attributes:
+        print("{0}: {1}".format(key, instance[key]))
+    print("------")
